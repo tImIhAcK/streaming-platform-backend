77a14248d566d4f27a2076db344baf2576e4ffe4
Adeniran John <adeniranjohn2016@gmail.com>
2025-12-01 20:08:54 +0100

    Added Depends

--- app/core/role_checker.py
+++ app/core/role_checker.py
@@ -1,6 +1,8 @@
+from fastapi import Depends
+
 from app.core.deps import RoleChecker
 
-admin_role_checker = RoleChecker(allowed_roles=["admin"])
-streamer_role_checker = RoleChecker(allowed_roles=["streamer"])
-viewer_role_checker = RoleChecker(allowed_roles=["viewer"])
-moderator_role_checker = RoleChecker(allowed_roles=["moderator"])
+admin_role_checker = Depends(RoleChecker(allowed_roles=["admin"]))
+streamer_role_checker = Depends(RoleChecker(allowed_roles=["streamer"]))
+viewer_role_checker = Depends(RoleChecker(allowed_roles=["viewer"]))
+moderator_role_checker = Depends(RoleChecker(allowed_roles=["moderator"]))
